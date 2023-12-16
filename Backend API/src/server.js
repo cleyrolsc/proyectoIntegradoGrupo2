11866dfcbd7519c559d4d00a5b2b2d3a94c16ea1
Djidjelly Siclait <djidjellypas92@gmail.com>
2023-12-16 00:15:45 -0400

    Crear controlador de usuarios

--- Backend API/src/server.js
+++ Backend API/src/server.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const { urlencoded } = require("express");
+
+const UsersRouter = require("./Controllers/Users/users.route");
+
 const app = express();
+app.set('port', process.env.PORT || 3000);
+
+//#region Middleware
+
+app.use(express.json());
+app.use(urlencoded({ extended: true}));
+
+//#endregion
 
 app.get('/', (req, res) => {
 
     res.send('Hola, Mundo! Esto es el Backend API');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server Started. Port: ${PORT}`);
+app.use('/api/users', UsersRouter);
+
+app.listen(app.get('port'), () => {
+    console.log(`Server Started. Port: ${app.get('port')}`);
 });
