7359c945d25ffe30fbb66d68da176fe252012027
Djidjelly Siclait <djidjellypas92@gmail.com>
2023-12-20 16:27:34 -0400

    Crear controlador de administracion con endpoint para registrar nuevos usuarios y admin

--- Backend API/src/Controllers/Admin/admin.controller.js
+++ Backend API/src/Controllers/Admin/admin.controller.js
@@ -1,11 +1,39 @@
-
 const { CreateUserRequest } = require("../../Core/Abstractions/Contracts/Requests");
 const { UserType } = require("../../Core/Abstractions/Enums");
 const { BadRequestError, NotFoundError } = require("../../Core/Abstractions/Exceptions");
+const { isNullOrUndefined } = require("../../Core/Utils/null-checker.util");
 
 const UsersService = require("../../Services/Users/users.service");
 const PrivilegesService = require("../../Services/Privileges/privileges.service");
-const { isNullOrUndefined } = require("../../Core/Utils/null-checker.util");
+
+const registerAdminUser = (request, response) => {
+    let { type, privilegeLevel: privilege } = request.body;
+    if (type < UserType.Accounting) {
+        return response.status(400).send("Administrative users cannot be of type lower than 98.");
+    }
+
+    let existingPrivilege = PrivilegesService.getPrivilege(privilege);
+    let adminPrivileges = getAdminPrivileges();
+    if (!adminPrivileges.includes(existingPrivilege.name)) {
+        return response.status(400).send("Administrative users cannot have privilege levels lower than 98.");
+    }
+
+    let createUserRequest = new CreateUserRequest(request.body);
+    let newAdmin = UsersService.registerNewUser(createUserRequest);
+
+    response.status(201).json(newAdmin);
+};
+
+function getAdminPrivileges() {
+    let privileges = PrivilegesService.getPrivilegesByLevel(98);
+
+    let adminPrivileges = [];
+    privileges.forEach(privilege => {
+        adminPrivileges.push(privilege.name);
+    });
+
+    return adminPrivileges;
+}
 
 const registerNonAdminUser = (request, response) => {
     let { type, privilegeLevel: privilege } = request.body;
@@ -14,12 +42,7 @@ const registerNonAdminUser = (request, response) => {
     }
 
     let existingPrivilege = PrivilegesService.getPrivilege(privilege);
-    let privileges = PrivilegesService.getPrivilegesByLevel(98);
-
-    let adminPrivileges = [];
-    privileges.forEach(privilege => {
-        adminPrivileges.push(privilege.name);
-    });
+    let adminPrivileges = getAdminPrivileges();
     if (adminPrivileges.includes(existingPrivilege.name)) {
         return response.status(400).send("Non administrative users cannot have privilege levels of 98 or higher.");
     }
@@ -31,5 +54,6 @@ const registerNonAdminUser = (request, response) => {
 };
 
 module.exports = {
-    registerNonAdminUser
+    registerAdminUser,
+    registerNonAdminUser,
 };
--- Backend API/src/Controllers/Admin/admin.route.js
+++ Backend API/src/Controllers/Admin/admin.route.js
@@ -4,6 +4,7 @@ const AdminController = require('./admin.controller');
 
 const adminRouter = express.Router();
 
+adminRouter.post('/register-admin', AdminController.registerAdminUser);
 adminRouter.post('/register-employee', AdminController.registerNonAdminUser);
 
 module.exports = adminRouter;
