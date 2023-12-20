8f53ebac10b1c439878a26b180da70154343ccd9
Djidjelly Siclait <djidjellypas92@gmail.com>
2023-12-19 22:41:08 -0400

    Agregar referencia a repositorio de privilegios

--- Backend API/src/Repositories/index.js
+++ Backend API/src/Repositories/index.js
@@ -1,7 +1,9 @@
 const EmployeesRepository = require("./employees.repository");
+const PrivilegesRepository = require("./privileges.repository");
 const UsersRepositories = require("./users.repository");
 
 module.exports = {
     EmployeesRepository,
+    PrivilegesRepository,
     UsersRepositories
 };
