733336ba5a48f6cdd5b07a46dc73ea491175ad1d
Djidjelly Siclait <djidjellypas92@gmail.com>
2023-12-19 22:29:12 -0400

    Crear repositorio de privilegios

---
+++ Backend API/src/Repositories/privileges.repository.js
@@ -0,0 +1,83 @@
+const { isListEmpty, isNotNullNorUndefined } = require("../Core/Utils/null-checker.util");
+const { PrivilegeStatus } = require("../Core/Abstractions/Enums");
+
+const DatabaseManager = require("../Database/database");
+
+const tableName = "privileges";
+
+const createPrivilege = (name, level) => {
+        // Validate data
+
+        let today = new Date();
+        let values = `'${name}', '${+level}', '${+PrivilegeStatus.Active}', '${today.toString()}'`;
+    
+        return DatabaseManager.run(`INSERT INTO ${tableName} (name, level, status, createdOn) VALUES (${values})`);
+};
+
+const getPrivilegeByName = (name) => {
+    let privileges = DatabaseManager.query(`SELECT * FROM ${tableName} WHERE name = ${name} LIMIT 1`);
+    if (isListEmpty(privileges)) {
+        return undefined;
+    }
+
+    return privileges[0];
+};
+
+const getPrivileges = (filterByName = undefined, skip = 0, limit = 10, orderBy = "DESC") => {
+    let whereClause = isNotNullNorUndefined(filterByName) ? `WHERE name LIKE '%${filterByName}%'`: "";
+
+    let privileges = DatabaseManager.query(`SELECT * FROM ${tableName} ${whereClause} ORDERBY name ${orderBy} OFFSET ${+(skip * limit)} LIMIT ${+limit}`);
+
+    return privileges;
+};
+
+const updatePrivilege = (name, {level = undefined, status = undefined}) => {
+    let privilege = getPrivilegeByName(name);
+    if (isNullOrUndefined(privilege)) {
+        return undefined;
+    }
+
+    let params = generateUpdateParameters();
+
+    if (params === "") {
+        return getPrivilegeByName(name);
+    }
+
+    let result = DatabaseManager.run(`UPDATE ${tableName} SET ${params} WHERE name = ${name}`);
+    if (result.changes === 0) {
+        throw new FatalError(`Unable to update privilege '${name}'`);
+    }
+
+    return getPrivilegeByName(name);
+
+    function generateUpdateParameters() {
+        let params = "";
+
+        if (isNotNullNorUndefined(level)) {
+            params += `level = ${+level}`;
+        }
+
+        if (isNotNullNorUndefined(status)) {
+            params += params !== "" ? ", " : params;
+
+            params += `status = ${+status}`;
+        }
+
+        let today = new Date();
+        params += params !== "" ? `, modifiedOn = '${today.toString()}'` : `modifiedOn = '${today.toString()}'`;
+        
+        return params;
+    }
+};
+
+const deletePrivilege = (name) => {
+    throw new Error("Not Implemented");
+}
+
+module.exports = {
+    createPrivilege,
+    getPrivilegeByName,
+    getPrivileges,
+    updatePrivilege,
+    deletePrivilege
+};
--- Backend API/src/Repositories/users.repository.js
+++ Backend API/src/Repositories/users.repository.js
@@ -1,4 +1,3 @@
-const User = require("./Entities/user.class");
 const { UserStatus, UserType } = require("../Core/Abstractions/Enums");
 const { isNotNullNorUndefined, isNullOrUndefined, isListEmpty } = require("../Core/Utils/null-checker.util");
 
@@ -26,8 +25,8 @@ const getUserByUsername = (username) => {
     return users[0];
 };
 
-const getUsers = (skip = 0, limit = 10, order = 'DESC') => {
-    let users = DatabaseManager.query(`SELECT * FROM ${tableName} ORDER BY username ${order} OFFSET ${+(skip * limit)} LIMIT ${+limit}`);
+const getUsers = (skip = 0, limit = 10, orderBy = 'DESC') => {
+    let users = DatabaseManager.query(`SELECT * FROM ${tableName} ORDER BY username ${orderBy} OFFSET ${+(skip * limit)} LIMIT ${+limit}`);
 
     return users;
 };
