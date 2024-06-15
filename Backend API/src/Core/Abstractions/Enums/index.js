d8d24925760bcdc5433a1b2ba407a278045bb435
Djidjelly Siclait <darnault@intellisys.com.do>
2024-06-15 16:46:36 -0400

    Modificar repositorio de empleados

--- Backend API/src/Core/Abstractions/Enums/index.js
+++ Backend API/src/Core/Abstractions/Enums/index.js
@@ -1,10 +1,8 @@
-const Department = require("./department.enum");
 const PrivilegeStatus = require("./privilege-status.enum");
 const UserStatus = require("./user-status.enum");
 const UserType = require("./user-type.enum");
 
 module.exports = {
-    Department,
     PrivilegeStatus,
     UserStatus,
     UserType,
