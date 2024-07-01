e0e9f8a856bf401a98446bfcb45f533b1e5bf762
Djidjelly Siclait <darnault@intellisys.com.do>
2024-06-30 22:45:41 -0400

    Refactor reportedHour entity into a sequelize model

--- Backend API/src/Repositories/Entities/reported-hour.class.js
+++ Backend API/src/Repositories/Entities/reported-hour.class.js
@@ -1,8 +1,30 @@
-class ReportedHour{
-    id; // PK
-    eventDate;
-    eventId; // FK Events
-    employeeId; // FK Employees
-};
+const { DataTypes, Model } = require('sequelize');
+
+const dbContext = require('../../Database/db-config');
+class ReportedHour extends Model{};
+
+ReportedHour.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+    },
+    eventDate: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    eventId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    employeeId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+}, {
+    dbContext,
+    modelName: 'ReportedHour',
+    tableName: 'reportedHours'
+});
 
 module.exports = ReportedHour;
