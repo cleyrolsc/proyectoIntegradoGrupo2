e8919339993d144056a89f1eb76ebf856dba1e79
Djidjelly Siclait <darnault@intellisys.com.do>
2024-07-01 14:05:26 -0400

    Refactor department entity into a sequelize model

--- Backend API/src/Repositories/Entities/department.class.js
+++ Backend API/src/Repositories/Entities/department.class.js
@@ -1,6 +1,25 @@
-class Department{
-    id; //PK
-    description;
-};
+const { DataTypes, Model } = require('sequelize');
+
+const dbContext = require('../../Database/db-config');
+
+class Department extends Model{};
+
+Department.init({
+    id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    }
+}, {
+    dbContext,
+    modelName: 'Department',
+    tableName: 'departments',
+    timestamps: false,
+});
 
 module.exports = Department;
