f846871776928ce783fc2a96e576c1ca633015e9
Djidjelly Siclait <darnault@intellisys.com.do>
2024-06-30 20:54:50 -0400

    Refactor user entity into a sequelize model

--- Backend API/src/Repositories/Entities/user.class.js
+++ Backend API/src/Repositories/Entities/user.class.js
@@ -1,13 +1,55 @@
-const ICreatedModifiedOn = require("../../Core/Abstractions/Interfaces/i-created-modified-on.interface");
+const { DataTypes, Model } = require('sequelize');
 
-class User extends ICreatedModifiedOn {
-    username; // PK
-    employeeId; //FK Employee
-    password;
-    type; // UserType.Agent (enum)
-    privilegeId; // FK Privilege
-    privilegeSuspended = false;
-    status; // UserStatus.Active (enum)
-}
+const dbContext = require('../../Database/db-config');
+const { UserType, UserStatus } = require("../../Core/Abstractions/Enums");
+
+class User extends Model {};
+
+User.init({
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        primaryKey: true
+    },
+    employeeId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    type: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: UserType.Agent
+    },
+    privilegeId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    privilegeSuspended: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    status: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: UserStatus.Active
+    },
+    createdOn: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    modifiedOn: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+}, {
+    dbContext,
+    modelName: 'User',
+    tableName: 'Users'
+});
 
 module.exports = User;
