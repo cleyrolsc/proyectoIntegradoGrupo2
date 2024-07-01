294da6eae1221da0f552ff7caf1a1769fdc5912b
Djidjelly Siclait <darnault@intellisys.com.do>
2024-07-01 14:11:33 -0400

    Refactor employee entity into a sequelize model

--- Backend API/src/Repositories/Entities/employee.class.js
+++ Backend API/src/Repositories/Entities/employee.class.js
@@ -1,14 +1,65 @@
-const ICreatedModifiedOn = require("../../Core/Abstractions/Interfaces/i-created-modified-on.interface");
+const { DataTypes, Model } = require('sequelize');
 
-class Employee extends ICreatedModifiedOn {
-    id; // PK
-    firstName;
-    lastName;
-    identificationNumber;
-    payPerHour = 0.0;
-    departmentId; //FK Departments
-    supervisorId; // FK Employees 
-    positionId; // FK Positions
-};
+const dbContext = require('../../Database/db-config');
+const Department = require('./department.class');
+const Position = require('./position.class');
+
+class Employee extends Model {};
+
+Employee.init({
+    id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    identificationNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    payPerHour: {
+        type: DataTypes.DOUBLE,
+        defaultValue: 0.0,
+    },
+    departmentId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Department,
+            key: 'id',
+            deferrable: Deferrable.INITIALLY_IMMEDIATE
+        }
+    },
+    supervisorId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Employee,
+            key: 'id',
+            deferrable: Deferrable.INITIALLY_IMMEDIATE
+        }
+    }, 
+    positionId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Position,
+            key: 'id',
+            deferrable: Deferrable.INITIALLY_IMMEDIATE
+        }
+    }
+}, {
+    dbContext,
+    modelName: 'Employee',
+    tableName: 'employees',
+    timestamps: true,
+});
 
 module.exports = Employee;
