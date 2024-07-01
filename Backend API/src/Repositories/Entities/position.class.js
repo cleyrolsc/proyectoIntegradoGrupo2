9e5808ee5a50ebc09b1fba6d781d16b54316f6f8
Djidjelly Siclait <darnault@intellisys.com.do>
2024-07-01 14:10:19 -0400

    Refactor position entity into a sequelize model

--- Backend API/src/Repositories/Entities/position.class.js
+++ Backend API/src/Repositories/Entities/position.class.js
@@ -1,6 +1,25 @@
-class Position{
-    id; //PK
-    description;
-};
+const { DataTypes, Model } = require('sequelize');
+
+const dbContext = require('../../Database/db-config');
+
+class Position extends Model{};
+
+Position.init({
+    id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    }
+}, {
+    dbContext,
+    modelName: 'Position',
+    tableName: 'positions',
+    timestamps: false,
+});
 
 module.exports = Position;
