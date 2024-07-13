0168fc13a7c49e5868ac65e8770605b7dbe67ffc
Cleyrol S <cleyrolsc@gmail.com>
2024-07-13 13:47:16 -0400

    Create login functionality and add users data

--- Frontend API/scripts/app.js
+++ Frontend API/scripts/app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+import { users } from '/data/users.js';
+import { currentUser } from './login';
+
 const btnApprove = document.querySelector('.approve');
 const btnStartShift = document.querySelector('.start');
 const disputeModal = document.querySelector('.dispute-modal');
@@ -73,75 +76,3 @@ btnStopWorking.addEventListener('click', () => {
   btnStartShift.classList.remove('hidden');
   logoutEl.textContent = 'Logout';
 });
-
-// let timerId;
-// let seconds = 0;
-// let minutes = 0;
-// let hours = 0;
-// const btnstart = document.getElementById("mainbtn");
-// const paused = document.getElementById("paused");
-// const timer = document.getElementById("timer");
-
-// function pad(number) {
-//   return number < 10 ? "0" + number : number;
-// }
-
-// function updateTimer() {
-//   seconds++;
-//   if (seconds >= 60) {
-//     seconds = 0;
-//     minutes++;
-//     if (minutes >= 60) {
-//       minutes = 0;
-//       hours++;
-//     }
-//   }
-//   timer.textContent = `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
-// }
-
-// btnstart.addEventListener("click", () => {
-//   if (btnstart.textContent == "Start") {
-//     btnstart.style.backgroundColor = "rgb(255, 193, 7)";
-//     btnstart.textContent = "Pause";
-//     timer.style.display = "block";
-//     timerId = setInterval(updateTimer, 1000);
-//   } else if (btnstart.textContent == "Pause") {
-//     paused.style.display = "block";
-//     btnstart.style.display = "none";
-//     clearInterval(timerId);
-//   }
-// });
-
-// const btnStartShift = document.querySelector('.start-button');
-// const timerEl = document.getElementById('timer');
-// const btnBreaks = document.querySelector('.non-working-buttons');
-// const btnBreak = document.querySelector('.break');
-// const btnLunch = document.querySelector('.lunch');
-// const btnCoachingTraining = document.querySelector('.coaching-training');
-// const logoutEl = document.querySelector('.logout');
-// const navList = document.querySelector('.nav-list');
-// const confirmationFormEl = document.querySelector('.confirmation-form');
-
-// logoutEl.addEventListener('click', function (event) {
-//   event.preventDefault();
-//   // disputeModalEl.style.display = 'none';
-//   confirmationFormEl.style.display = 'flex';
-//   document.querySelector('body').classList.add('overlay');
-//   btnStartShift.classList.add('hidden');
-//   navList.classList.add('hidden');
-//   btnBreaks.classList.add('hidden');
-
-//   // disputeFormEl.classList.remove('hidden');
-// });
-
-// btnStartShift.addEventListener('click', function () {
-//   if (btnStartShift.textContent === 'Pause') {
-//     btnBreaks.classList.remove('hidden');
-//     btnStartShift.classList.add('hidden');
-//   }
-//   logoutEl.textContent = 'Stop working';
-//   btnStartShift.style.backgroundColor = '#ffc107';
-//   btnStartShift.textContent = 'Pause';
-// });
-
-// });
