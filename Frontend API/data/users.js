12104843ecd5aa80ae25f90d767d1e018cdbb13a
Cleyrol S <cleyrolsc@gmail.com>
2024-07-13 13:47:16 -0400

    Create login functionality and add users data

---
+++ Frontend API/data/users.js
@@ -0,0 +1,35 @@
+export { users };
+const users = [
+  {
+    name: 'Cleyrol',
+    lastName: 'Santana',
+    id: '001',
+    department: 'Operations',
+    email: 'cleyrol@tms.com',
+    password: 'cleyrol1234',
+  },
+  {
+    name: 'Erick',
+    lastName: 'Sencion',
+    id: '001',
+    department: 'Training',
+    email: 'erick@tms.com',
+    password: 'erick1234',
+  },
+  {
+    name: 'Djidjelly',
+    lastName: 'Siclait',
+    id: '001',
+    department: 'Developer',
+    email: 'djidjelly@tms.com',
+    password: 'djidjelly1234',
+  },
+  {
+    name: 'Gerar',
+    lastName: 'Almonte',
+    id: '001',
+    department: 'DevOps',
+    email: 'gerar@tms.com',
+    password: 'gerar1234',
+  },
+];
