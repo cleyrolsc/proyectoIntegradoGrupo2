10ffc0415fe7b59fa349003e00fcbf0cc4244622
Djidjelly Siclait <darnault@intellisys.com.do>
2024-07-18 18:56:35 -0400

    Add getPrivilegesAsync endpoint

--- Backend API/src/Controllers/Admin/admin.route.js
+++ Backend API/src/Controllers/Admin/admin.route.js
@@ -5,12 +5,10 @@ const AdminController = require('./admin.controller');
 
 const adminRouter = express.Router();
 
+adminRouter.get('/privileges', checkForAdminPrivileges, AdminController.getPrivilegesAsync);
 
-
-//adminRouter.use('/register-admin', );
 adminRouter.post('/register-admin', checkForAdminPrivileges, AdminController.registerAdminUserAsync);
 
-//adminRouter.use('/register-user', checkForAdminPrivileges);
 adminRouter.post('/register-user', checkForAdminPrivileges, AdminController.registerUserAsync);
 
 module.exports = adminRouter;
