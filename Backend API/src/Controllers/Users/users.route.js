c20cb1c60444b966c9edda3cc165c800ced3efa8
Djidjelly Siclait <darnault@intellisys.com.do>
2024-07-18 14:43:54 -0400

    Add checkForAdminPrivileges middleware to restrict access to users with incorrect privilege levels

--- Backend API/src/Controllers/Users/users.route.js
+++ Backend API/src/Controllers/Users/users.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const { checkForAdminPrivileges } = require('../../Core/Filters/privilege-checks.filter');
 
 const UsersController = require('./users.controller');
 
 const usersRouter = express.Router();
 
-usersRouter.get('/', UsersController.fetchAllUsersAsync);
+usersRouter.get('/', checkForAdminPrivileges, UsersController.fetchAllUsersAsync);
 
 usersRouter.get('/:username/profile', UsersController.viewProfileAsync);
 
