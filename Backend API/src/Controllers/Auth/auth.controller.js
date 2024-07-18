048115cc3d39b30a43a8b74bb9d0a0f61d99c92d
Djidjelly Siclait <darnault@intellisys.com.do>
2024-07-18 10:42:43 -0400

    Create AuthController and AuthRouter

---
+++ Backend API/src/Controllers/Auth/auth.controller.js
@@ -0,0 +1,15 @@
+const UsersService = require("../../Services/Users/users.service");
+const PrivilegesService = require("../../Services/Privileges/privileges.service");
+
+const loginAsync = async (request, response, next) => {
+
+};
+
+const logoutAsync = async (request, response, next) => {
+
+};
+
+module.exports = {
+    loginAsync,
+    logoutAsync
+};
