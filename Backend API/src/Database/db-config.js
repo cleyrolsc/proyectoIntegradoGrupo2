82bd6f90964a2f8b0427d3e725eadbe2dc5adfd5
Djidjelly Siclait <djidjellypas92@gmail.com>
2024-08-05 22:16:40 -0400

    Fetch sequelize configuration from environment variables

--- Backend API/src/Database/db-config.js
+++ Backend API/src/Database/db-config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const { MySqlDialect } = require('@sequelize/mysql');
 
@@ -7,12 +8,12 @@ const { MySqlDialect } = require('@sequelize/mysql');
   });*/
 
 const dbContext = new Sequelize({
-    dialect: 'mysql',
-    database: 'backend-db',
-    username: 'backend-api',
-    password: 'NN.PyI@aIZaDK)k(',
-    host: 'localhost',
-    port: 3307
+    dialect: process.env.DB_DIALECT,
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT
   });
    
 dbContext.authenticate()
@@ -21,14 +22,5 @@ dbContext.authenticate()
 }).catch(error => {
   console.log(`Error: ${error}`);
 })
-
-async function testConnection() {
-    try {
-        await db.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
-}
 
 module.exports = dbContext;
