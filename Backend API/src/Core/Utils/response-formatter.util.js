b422840535f4ba50104629db38103aa9c8493b1d
Djidjelly Siclait <djidjellypas92@gmail.com>
2024-08-05 21:52:44 -0400

    Add formatErrorResponse and update swagger

--- Backend API/src/Core/Utils/response-formatter.util.js
+++ Backend API/src/Core/Utils/response-formatter.util.js
@@ -7,4 +7,20 @@ const formatResponse = (statusCode = 200, path, content) => {
     };
 };
 
-module.exports = formatResponse;
+const formatErrorResponse = (statusCode = 200, path, error) => {
+    return {
+        statusCode,
+        path,
+        timestamp: new Date().toISOString(),
+        content: {
+            errorType: error.constructor.name,
+            message: error.message,
+            //stack: error.stack
+        }
+    };
+};
+
+module.exports = {
+    formatResponse,
+    formatErrorResponse
+};
