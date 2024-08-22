9b149d4c9202d4487d22e7b0f7b6cc5bcc9d4a1e
Djidjelly Siclait <darnault@intellisys.com.do>
2024-08-21 20:50:37 -0400

    Add get all incidents submitted by one employee endpoint

--- Backend API/src/Services/Incidents/incidents.service.js
+++ Backend API/src/Services/Incidents/incidents.service.js
@@ -1,22 +1,27 @@
 const { isNullUndefinedOrEmpty, isNullOrUndefined } = require('../../Core/Utils/null-checker.util');
-const { BadRequestError } = require('../../Core/Abstractions/Exceptions');
-
-const IncidentsRepository = require('../../Repositories/incidents.repository');
-const EmployeesRepository = require('../../Repositories/employees.repository');
+const { BadRequestError, NotFoundError } = require('../../Core/Abstractions/Exceptions');
 const { PaginatedResponse } = require('../../Core/Abstractions/Contracts/Responses');
 const { formatPaginatedResponse } = require('../../Core/Utils/response-formatter.util');
 
+const IncidentsRepository = require('../../Repositories/incidents.repository');
+const EmployeesRepository = require('../../Repositories/employees.repository');
+
 const registerIncidentAsync = async (employeeId, comment) => {
-    let employee = await EmployeesRepository.getEmployeeByIdAsync(employeeId);
-    if (isNullOrUndefined(employee)) {
-        throw new BadRequestError('Employee id cannot be undefined');
-    }
+    let employee = await checkIfEmployeeExistAsync(employeeId);
     
     if(isNullUndefinedOrEmpty(comment)) {
         throw new BadRequestError('Comment cannot be empty');
     }
 
     return IncidentsRepository.createIncidentAsync(employee.id, employee.supervisorId, comment);
+};
+
+async function checkIfEmployeeExistAsync(employeeId) {
+    let employee = await EmployeesRepository.getEmployeeByIdAsync(employeeId);
+    if (isNullOrUndefined(employee)) {
+        throw new NotFoundError(`Employee with id, ${employeeId}, cannot be found`);
+    }
+    return employee;
 };
 
 const getIncidentByIdAsync = async (id) => {
@@ -68,10 +73,46 @@ const getIncidentsAsync = async (currentPage = 1, itemsPerPage = 10, order = 'DE
     });
 
     return formatPaginatedResponse(currentPage, itemsPerPage, incidentModels, count);
-}
+};
+
+const getIncidentByEmployeeIdAsync = async (employeeId, currentPage = 1, itemsPerPage = 10, order = 'DESC') => {
+    let employee = await checkIfEmployeeExistAsync(employeeId);
+
+    let skip = (currentPage - 1) * itemsPerPage;
+    let { count, rows:incidents } = await IncidentsRepository.getIncidentsByEmployeeIdAsync(employeeId, skip, itemsPerPage, order);
+    if (count === 0) {
+        return new PaginatedResponse();
+    }
+
+    let supervisor = await EmployeesRepository.getEmployeeByIdAsync(employee.supervisorId);
+    
+    let incidentModels = [];
+    incidents.forEach(entity => {
+        let { id, employeeId, supervisorId, comment, status, createdAt } = entity;
+        incidentModels.push({
+            id,
+            submittedBy: {
+                id: employeeId,
+                firstName: employee.firstName,
+                lastName: employee.lastName
+            },
+            comment,
+            submittedTo: {
+                id: supervisorId,
+                firstName: supervisor.firstName,
+                lastName: supervisor.lastName
+            },
+            submittedOn: createdAt,
+            status: status
+        });
+    });
+
+    return formatPaginatedResponse(currentPage, itemsPerPage, incidentModels, count);
+};
 
 module.exports = {
     registerIncidentAsync,
     getIncidentByIdAsync,
-    getIncidentsAsync
+    getIncidentsAsync,
+    getIncidentByEmployeeIdAsync
 };
